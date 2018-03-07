300ca59fbd6737b1239b16aaec86447bf2791e83
Carre Perkins <perki107@mail.chapman.edu>
2018-03-06 21:10:35 -0800

    adding

--- ChatterBot.py
+++ ChatterBot.py
@@ -4,10 +4,11 @@ import json
 import time
 
 # Twitter API Keys
-consumer_key = "Ed4RNulN1lp7AbOooHa9STCoU"
-consumer_secret = "P7cUJlmJZq0VaCY0Jg7COliwQqzK0qYEyUF9Y0idx4ujb3ZlW5"
-access_token = "839621358724198402-dzdOsx2WWHrSuBwyNUiqSEnTivHozAZ"
-access_token_secret = "dCZ80uNRbFDjxdU2EckmNiSckdoATach6Q8zb7YYYE5ER"
+consumer_key = "FQ77UMLjKRgcoFSyTASzwixnI"
+consumer_secret = "1qVi8JYhEUU3xAWVnrL99AUznEWOIuQWKZH8J1KkxWjtC24qL0"
+access_token = "969400125805047808-yAN8pR7wneqdTr3CW4of1651vJmU5IV"
+access_token_secret = "vuxPy8mfIk3XcIUPoczSFUAfgvEtaQNpZ1q843miRPNJ3"
+
 
 # Setup Tweepy API Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -16,4 +17,4 @@ api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
 
 # Create a function that tweets
-# CODE GOES HERE
+# CODE GOES HERES
